f7d713d19d018e56847007052527f6a97d927e4f
Roland Crompton <rollycrompton@gmail.com>
2022-12-03 12:00:36 +0000

    -Implemented draw() method for class Player

--- src/pij/main/Player.java
+++ src/pij/main/Player.java
@@ -1,5 +1,23 @@
 package pij.main;
 
-public abstract class Player {
+import java.util.ArrayList;
 
+public abstract class Player {
+	/** an array of tiles a player has available to them */
+	private ArrayList<Tile> tileRack = new ArrayList<>();
+	
+	/**
+	 * Prints the tile rack.
+	 */
+	public void printRack() {
+		System.out.println(tileRack.toString());
+	}
+	/**
+	 * Adds a tile to the tile rack.
+	 * 
+	 * @param tile tile to be added.
+	 */
+	public void draw(Tile tile) {
+		tileRack.add(tile);
+	}
 }
