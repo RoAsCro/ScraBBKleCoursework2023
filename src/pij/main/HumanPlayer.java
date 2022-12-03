df039bc55fa27bb175492f83f31aa8b91b520479
Roland Crompton <rollycrompton@gmail.com>
2022-12-03 12:17:47 +0000

    -Implemented printRack() for class HumanPlayer

--- src/pij/main/HumanPlayer.java
+++ src/pij/main/HumanPlayer.java
@@ -2,4 +2,17 @@ package pij.main;
 
 public class HumanPlayer extends Player {
 
+	
+	/**
+	 * Prints the tile rack.
+	 */
+	public void printRack() {
+		String separator = "";
+		for (Tile tile : getRack()) {
+			System.out.print(separator + "[" + tile.getText()+ "]");
+			separator = ", ";
+		}
+		System.out.println();
+	}
+	
 }
