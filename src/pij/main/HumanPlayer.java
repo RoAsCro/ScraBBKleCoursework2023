d7f0204e160718932620d9c7ee2106e24879f01b
Roland Crompton <rollycrompton@gmail.com>
2022-12-04 13:42:40 +0000

    -Fixed input validation bug in HumanPlayer

--- src/pij/main/HumanPlayer.java
+++ src/pij/main/HumanPlayer.java
@@ -19,11 +19,12 @@ public class HumanPlayer extends Player {
 	@Override
 	public Move turn(Bag bag) {
 		draw(bag);
-		printRack();
+		
 		String input = "";
 		Move move = null;
 		
 		while (move == null || !move.isValid()) {
+			printRack();
 			System.out.println(
 					"Please enter your move with letter sequence, position, "
 					+ "and direction(d for down, r for right) separated by commas. "
