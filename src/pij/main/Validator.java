033ea0f6dc8001e1d99275c94bf7afaff3d005c1
Mr Roland Crompton <rcromp01@dcs.bbk.ac.uk>
2022-12-05 14:26:25 +0000

    -Implemented a wordLookup method for class Validator

--- src/pij/main/Validator.java
+++ src/pij/main/Validator.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class Validator {
 	
-	private static TreeSet<String> dictionary;
+	private static TreeSet<String> dictionary = new TreeSet<>();
 	
 	public static void loadDictionary(File file) {
 		
@@ -23,6 +23,12 @@ public class Validator {
 			e.printStackTrace();
 		}
 
+	}
+	
+	public static boolean lookupWord(String word) {
+		if (dictionary.contains(word))
+			return true;
+		return false;
 	}
 	
 	public static Board loadFile(String fileName) {
@@ -71,111 +77,114 @@ public class Validator {
 		return false;
 	}
 	public static void main(String args[]) {
-		dictionary();
+		loadDictionary(new File("./resources/wordlist.txt"));
+		System.out.println(lookupWord("zygote"));
+		System.out.println(lookupWord("adwdaw"));
+		System.out.println(lookupWord("cat"));
 	}
-	public static void dictionary() {
-		File file = new File("./resources/wordlist.txt");
-		try (Scanner scanner = new Scanner(file)){
-			//Hashtable<String> table = new Hashtable<>();
-			TreeSet<String> tree = new TreeSet<>();
-			String search = "zymosis";
-			//int i = 0;
-			long a = System.nanoTime();
-			//System.out.println(scanner.findWithinHorizon(search, 2974773));
-//			while (scanner.hasNext()) {
-//				
-////				if (scanner.next().equals("zzzs")) {
-////					
+//	public static void dictionary() {
+//		File file = new File("./resources/wordlist.txt");
+//		try (Scanner scanner = new Scanner(file)){
+//			//Hashtable<String> table = new Hashtable<>();
+//			TreeSet<String> tree = new TreeSet<>();
+//			String search = "zymosis";
+//			//int i = 0;
+//			long a = System.nanoTime();
+//			//System.out.println(scanner.findWithinHorizon(search, 2974773));
+////			while (scanner.hasNext()) {
+////				
+//////				if (scanner.next().equals("zzzs")) {
+//////					
+//////					System.out.println("Found");
+//////					break;
+//////				}
+////				//System.out.println("A: " + scanner.next());
+////			}
+//			scanner.close();
+//			long b = System.nanoTime();
+//			System.out.println(b-a);
+//
+//			
+//			BufferedReader reader = new BufferedReader(new FileReader(file));
+//			int j = 0;
+//			String line;
+//			String[] array = new String[267753];
+//			
+//			while ((line = reader.readLine()) != null) {
+//				array[j] = line;
+//				tree.add(line);
+////				if (line.equals(search)) {
 ////					System.out.println("Found");
 ////					break;
 ////				}
-//				//System.out.println("A: " + scanner.next());
+//				//System.out.println("B: " + line);
+//				j++;
 //			}
-			scanner.close();
-			long b = System.nanoTime();
-			System.out.println(b-a);
-
-			
-			BufferedReader reader = new BufferedReader(new FileReader(file));
-			int j = 0;
-			String line;
-			String[] array = new String[267753];
-			
-			while ((line = reader.readLine()) != null) {
-				array[j] = line;
-				tree.add(line);
+//			a = System.nanoTime();
+//			System.out.println(tree.contains(search));
+//			b = System.nanoTime();
+//			System.out.println(b-a);
+//			
+//			a = System.nanoTime();
+//			boolean go = true;
+//			int f = 267752;
+//			int g = 0;
+//			int h = f;
+//			int n = 0;
+//			char c = search.charAt(0);
+//			while (n < search.length()) {
+//				h = g + ((f - g) / 2);
+//				//System.out.println(array[h]);
+//				//System.out.println(search.charAt(n));
+//				if (search.equals(array[h])) {
+//						System.out.println("Found!");
+//						break;
+//				}
+//				if (array[h].length() > n) {
+//					if (search.charAt(n) == array[h].charAt(n)) {
+//						n++;
+//					}
+//					else if (search.charAt(n)  < array[h].charAt(n)) {
+//						//System.out.println("B");
+//						f = h;
+//					} else if (search.charAt(n)  > array[h].charAt(n)) {
+//						//System.out.println("A");
+//						g = h;
+//					}
+//				} else g++;	
+//			}
+//			b = System.nanoTime();
+//			long x = b-a;
+//			System.out.println(b-a);
+//			reader.close();
+//			
+//			BufferedReader newreader = new BufferedReader(new FileReader(file));
+//			a = System.nanoTime();
+//			while ((line = newreader.readLine()) != null) {		
 //				if (line.equals(search)) {
-//					System.out.println("Found");
+//					//System.out.println("Found");
 //					break;
 //				}
-				//System.out.println("B: " + line);
-				j++;
-			}
-			a = System.nanoTime();
-			System.out.println(tree.contains(search));
-			b = System.nanoTime();
-			System.out.println(b-a);
-			
-			a = System.nanoTime();
-			boolean go = true;
-			int f = 267752;
-			int g = 0;
-			int h = f;
-			int n = 0;
-			char c = search.charAt(0);
-			while (n < search.length()) {
-				h = g + ((f - g) / 2);
-				//System.out.println(array[h]);
-				//System.out.println(search.charAt(n));
-				if (search.equals(array[h])) {
-						System.out.println("Found!");
-						break;
-				}
-				if (array[h].length() > n) {
-					if (search.charAt(n) == array[h].charAt(n)) {
-						n++;
-					}
-					else if (search.charAt(n)  < array[h].charAt(n)) {
-						//System.out.println("B");
-						f = h;
-					} else if (search.charAt(n)  > array[h].charAt(n)) {
-						//System.out.println("A");
-						g = h;
-					}
-				} else g++;	
-			}
-			b = System.nanoTime();
-			long x = b-a;
-			System.out.println(b-a);
-			reader.close();
-			
-			BufferedReader newreader = new BufferedReader(new FileReader(file));
-			a = System.nanoTime();
-			while ((line = newreader.readLine()) != null) {		
-				if (line.equals(search)) {
-					//System.out.println("Found");
-					break;
-				}
-				//System.out.println("B: " + line);
-			}
-			b = System.nanoTime();
-			long y = b-a;
-			System.out.println(b-a);
-			System.out.println();
-			System.out.println((y-x));
-			reader.close();
-			
-//			String x = scanner.findWithinHorizon("aard", 300);
-//			System.out.print(x);
-			
-			
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException ex) {
-			System.out.print("Woo");
-		}
-	}
+//				//System.out.println("B: " + line);
+//			}
+//			b = System.nanoTime();
+//			long y = b-a;
+//			System.out.println(b-a);
+//			System.out.println();
+//			System.out.println((y-x));
+//			reader.close();
+//			
+////			String x = scanner.findWithinHorizon("aard", 300);
+////			System.out.print(x);
+//			
+//			
+//			
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException ex) {
+//			System.out.print("Woo");
+//		}
+//	}
 	
 }
