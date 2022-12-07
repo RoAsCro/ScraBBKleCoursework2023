9b199489ea2dda462f684aef9fcdb8944005cf2a
Roland Crompton <rollycrompton@gmail.com>
2022-12-07 12:22:16 +0000

    -Fixed new Bag constructor so that it adds WildTiles

--- src/pij/main/Bag.java
+++ src/pij/main/Bag.java
@@ -13,6 +13,7 @@ public class Bag {
 	
 	public Bag() {
 		this(new int[]{9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1,2});
+		System.out.println(bag.size());
 //		tileGenerator("A", 1, 9);
 //		tileGenerator("B", 3, 2);
 //		tileGenerator("C", 3, 2);
@@ -49,8 +50,10 @@ public class Bag {
 		for (; i < quantities.length && i < 26; i++) {
 			tileGenerator(ALPHABET[i][0], Integer.parseInt(ALPHABET[i][1]), quantities[i]);
 		}
-		if (i != quantities.length - 1) {
-			tileGenerator(ALPHABET[i][0], Integer.parseInt(ALPHABET[i][1]), quantities[i + 1]);
+		if (i != quantities.length) {
+			for (int j = 0; j < quantities[i]; j++) {
+				bag.add(new WildTile());
+			}
 		}
 	}
 	
