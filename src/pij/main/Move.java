73dd354dfd1d62762291a1b7035ca844f6f07180
Roland Crompton <rollycrompton@gmail.com>
2022-12-07 22:01:51 +0000

    -Consolidated multiple if statements into if statement in the constructor

--- src/pij/main/Move.java
+++ src/pij/main/Move.java
@@ -30,54 +30,71 @@ public class Move {
 		if (input.equals(",,"))
 			PASS = true;
 		else {
-			
-			String[] movesToTest = input.split(",");
-			ArrayList<LetterTile> rack = new ArrayList<>(player.getRack());
 			PASS = false;
 			
+			String[] movesToTest = input.split(",");
+			
+			ArrayList<LetterTile> rack = new ArrayList<>(player.getRack());
+			
 			if (movesToTest.length == 3) {
-				x = movesToTest[1].substring(0,1).toLowerCase();
-				y = movesToTest[1].substring(1);
+				
+				String letters = movesToTest[0];
+				String location = movesToTest[1];
 				direction = movesToTest[2].toLowerCase();
 				
-				int yLength = y.length();
-				//Check the x coord is a letter
-				if (x.toUpperCase() == x.toLowerCase()) {
+				if (location.length() < 2 || location.length() > 3)
 					valid = false;
-				}
-				//Check the y coord is no larger than 2 and no smaller than one, and is a number
-				else if (yLength > 2 || yLength < 1 || !Character.isDigit(y.charAt(0)) || !Character.isDigit(y.charAt(yLength - 1))) {
-					valid = false;
-				}
-				//Check the direction is a valid direction
-				else if (!Validator.inputValidation(direction, new String[] {"r", "d"})) {
-					valid = false;
-				}
-				char[] chars = movesToTest[0].toCharArray();
-				int counter = 0;
-				int target = chars.length;
-				
-				//Check the player has the required tiles
-				for (char c : chars) {
-					for (LetterTile t : rack) {
-						char tChar = t.getChar();
-						if (tChar == c 
-								|| (Character.isLowerCase(c) && WildTile.class.isInstance(t))) {
-							tiles.add(t);
-							rack.remove(t);
-							counter++;
-							if (WildTile.class.isInstance(t)) {
-								WildTile w = (WildTile) t;
-								w.setTempText(c);
+				else {
+					x = location.substring(0,1).toLowerCase();
+					y = location.substring(1);
+					
+					int yLength = y.length();
+					//Check the x coord is a letter
+					if (!Character.isLetter(x.charAt(0)) || !Character.isDigit(y.charAt(0))
+							|| !Character.isDigit(y.charAt(yLength - 1))
+							|| !Validator.inputValidation(direction, new String[] { "r", "d" })
+							|| letters.length() < 1) {
+						valid = false;
+					}
+//					//Check the y coord is a number
+//					else if (!Character.isDigit(y.charAt(0)) || !Character.isDigit(y.charAt(yLength - 1))) {
+//						valid = false;
+//					}
+//					//Check the direction is a valid direction
+//					else if (!Validator.inputValidation(direction, new String[] {"r", "d"})) {
+//						valid = false;
+//					} 
+//					//Check there is at least one letter
+//					else if (letters.length() < 1) {
+//						valid = false;
+//					}
+					else {
+						char[] chars = letters.toCharArray();
+						int counter = 0;
+						int target = chars.length;
+						
+						//Check the player has the required tiles
+						for (char c : chars) {
+							for (LetterTile t : rack) {
+								char tChar = t.getChar();
+								if (tChar == c 
+										|| (Character.isLowerCase(c) && WildTile.class.isInstance(t))) {
+									tiles.add(t);
+									rack.remove(t);
+									counter++;
+									if (WildTile.class.isInstance(t)) {
+										WildTile w = (WildTile) t;
+										w.setTempText(c);
+									}
+									break;
+								}
 							}
-							break;
+						}
+						if (counter != target) {
+							valid = false;
 						}
 					}
 				}
-				if (counter != target) {
-					valid = false;
-				}
-			
 			}else
 				valid = false;
 		} 
