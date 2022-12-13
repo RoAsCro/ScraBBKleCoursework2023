9c7773b2b19fa87aa7bff7ad6e7c2704032897e0
Roland Crompton <rollycrompton@gmail.com>
2022-12-13 10:40:06 +0000

    -Finished implementing the depthFirstSearch for class BoardReader

--- src/pij/main/BoardReader.java
+++ src/pij/main/BoardReader.java
@@ -86,78 +86,36 @@ public class BoardReader {
 	public void depthFirstSearch(TileOperation method) {
 		currentX = board.getCentre();
 		currentY = board.getCentre();
-//		System.out.println("A: " + currentX + ", " + currentY);
 		depthFirstSearch(currentX, currentY, method);
 	}
 	
 	public boolean depthFirstSearch(int x, int y, TileOperation method) {
-//		Iterator<Tile> it = tileTree.descendingIterator();
-//		while (it.hasNext())
-//			System.out.println(it.next());
+
 		int treeRef = x * (board.getCentre() + 1) * 2 + y;
 		Tile tile = board.tileAt(x, y);
-		//System.out.println("A: " + tile.getText());
-		System.out.println("A: " + currentX + ", " + currentY);
+
 		if (this.tileTree.contains(treeRef)) {
-			System.out.println("J: " + currentX + ", " + currentY);
 			previous();
 			return false;
 		}
 
-		//System.out.println(tile.getText());
-		
 		if (!LetterTile.class.isInstance(tile)) {
-			System.out.println("K: " + currentX + ", " + currentY);
-			if (tile != null) tile.setText(" o ");
+			//if (tile != null) tile.setText(" o ");
 			method.execute(x, y);
 			previous();
 			return false;
 		}
-		tile.setText("X");
+		//tile.setText("X");
 		this.tileTree.add(treeRef);	
 		
-		next();
-//		try {
-//			Thread.sleep(1000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		if (depthFirstSearch(currentX, currentY, method)) {
-			
-			
+		for (int i = 0; i < 4; i++) {
+			next();
+			depthFirstSearch(currentX, currentY, method);
+			reverse();
+			if (i % 2 != 0)
+				turn();
 		}
-		System.out.println("B: " + currentX + ", " + currentY);
-		reverse();
-		next();
-		if (depthFirstSearch(currentX, currentY, method)) {
-			
-		}
-		System.out.println("C: " + currentX + ", " + currentY);
-		turn();
-		reverse();
-		next();
-		if (depthFirstSearch(currentX, currentY, method)) {
-			
-		}
-		System.out.println("D: " + currentX + ", " + currentY);
-		reverse();
-		next();
-		if (depthFirstSearch(currentX, currentY, method)) {
-			
-		}
-		System.out.println("E: " + currentX + ", " + currentY + " " + xInc + ", " + yInc);
-		
-		turn();
-		reverse();
 		previous();
-		System.out.println("E: " + currentX + ", " + currentY + " " + xInc + ", " + yInc);
-
-		for (Integer t : tileTree) {
-			int x1 = t / 16;
-			int y1 = t % 16;
-			System.out.print("(" + x1 + ", " + y1 + ") ");
-		}
 		System.out.println("TT: " + tileTree.size());
 		return true;
 	}
