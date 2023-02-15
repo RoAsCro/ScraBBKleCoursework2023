317e893f4a8346bb23e2d0aaf7e9bcbd990bcf9f
Roland Crompton <rollycrompton@gmail.com>
2023-02-15 11:23:39 +0000

    -Deleted the anagram dictionaries

--- src/pij/main/Validator.java
+++ src/pij/main/Validator.java
@@ -30,38 +30,11 @@ public class Validator {
 		return o1.compareTo(o2);
 	};
 
-	private static final Comparator<String> PRE_LEN_ANA_COMPARATOR = (o1, o2) -> {
-		int len1 = o1.length();
-		int len2 = o2.length();
-		if (!(len1 == len2))
-			return len1 - len2;
-
-		char[] chars = o1.toCharArray();
-		Arrays.sort(chars);
-		String orderedLine = new String(chars);
-		char[] chars2 = o2.toCharArray();
-		Arrays.sort(chars2);
-		String orderedLine2 = new String(chars2);
-		int comboCheck = orderedLine.compareTo(orderedLine2);
-		if (comboCheck != 0) {
-			return comboCheck;
-		}
-
-		int index = o1.indexOf(" ");
-		if (index != -1) {
-			o1 = o1.substring(0, index);
-			o2 = o2.substring(0, index);
-		}
-		return o1.compareTo(o2);
-	};
 
 
 	private static TreeSet<String> prefixDictionary = new TreeSet<>(PREFIX_COMPARATOR);
 
 	private static TreeSet<String> suffixDictionary = new TreeSet<>(PREFIX_COMPARATOR);
-
-	private static TreeSet<String> anagramDictionary = new TreeSet<>(PRE_LEN_ANA_COMPARATOR);
-	private static TreeSet<String> anagramDictionary2 = new TreeSet<>(PRE_LEN_ANA_COMPARATOR);
 
 	private static TreeSet<String> comboDictionary = new TreeSet<>();
 	public static TreeSet<String> lengthDictionary = new TreeSet<>((o1, o2) -> {
@@ -81,7 +54,6 @@ public class Validator {
 			while ((line = reader.readLine()) != null) {
 				dictionary.add(line);
 				suffixDictionary.add((new StringBuilder(line)).reverse().toString());
-				anagramDictionary2.add((new StringBuilder(line)).reverse().toString());
 				char[] chars = line.toCharArray();
 				Arrays.sort(chars);
 				String orderedLine = new String(chars);
@@ -94,10 +66,6 @@ public class Validator {
 		}
 		lengthDictionary.addAll(dictionary);
 		prefixDictionary.addAll(dictionary);
-		anagramDictionary.addAll(dictionary);
-
-
-
 	}
 	
 	public static void loadDictionary() {
@@ -112,13 +80,8 @@ public class Validator {
 		s = s.toLowerCase();
 		int dLength = dictionary.subSet(s.replaceAll(" ", "a"), true, s.replaceAll(" ", "z"), true).size();
 		int pLength = prefixDictionary.subSet(s.replaceAll(" ", "a"), true, s.replaceAll(" ", "z"), true).size();
-		int aLength = anagramDictionary.subSet(s.replaceAll(" ", "a"), true, s.replaceAll(" ", "z"), true).size();
-		int a2Length = anagramDictionary2.subSet(new StringBuilder(s.replaceAll(" ", "a")).reverse().toString(), true, new StringBuilder(s.replaceAll(" ", "z")).reverse().toString(), true).size();
-
 		System.out.println(dLength);
 		System.out.println(pLength);
-		System.out.println(aLength);
-		System.out.println(a2Length);
 
 	}
 
