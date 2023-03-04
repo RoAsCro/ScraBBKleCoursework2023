983c944f703f656142d9751d868c6c9b6447f248
Roland Crompton <rollycrompton@gmail.com>
2023-03-04 11:34:30 +0000

    Created PlayerTest

---
+++ src/test/PlayerTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pij.main.*;
+
+import java.util.List;
+
+public class PlayerTest {
+    @Test
+    public void testPlayer() {
+        Board board = TestUtility.loadBoardFromTestBoards("testBoard.txt");
+        Player player = new ComputerPlayer(board);
+
+        // Test score methods
+        Assertions.assertEquals(0, player.getScore());
+        player.updateScore(2);
+        Assertions.assertEquals(2, player.getScore());
+
+        // Test getBoard
+        Assertions.assertEquals(board.getCentre(), player.getBoard().getCentre());
+
+        // getRack and draw
+        Bag bag = new Bag(new int[]{1});
+        Assertions.assertTrue(player.getRack().isEmpty());
+        player.draw(bag);
+        Assertions.assertEquals('A', player.getRack().get(0).getChar());
+
+        // Remove
+        List<LetterTile> list = List.of(new LetterTile("A", 1));
+        player.removeTiles(list);
+        Assertions.assertTrue(player.getRack().isEmpty());
+
+    }
+
+}
