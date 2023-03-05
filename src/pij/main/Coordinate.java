092c2c16116ed269cfc4ad8bfedfef5aade5eac4
Roland Crompton <rollycrompton@gmail.com>
2023-03-05 12:55:48 +0000

    Coordinate.toString now displays in a human-readable way

--- src/pij/main/Coordinate.java
+++ src/pij/main/Coordinate.java
@@ -35,7 +35,7 @@ public class Coordinate implements Comparable<Coordinate> {
 
     @Override
     public String toString() {
-        return "(" + x + ", " + y + ")";
+        return "(" + ((char) x + ASCII_OF_LOWER_A) + ", " + (y + 1) + ")";
     }
 
     @Override
