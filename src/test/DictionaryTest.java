c1913c97df971e546277092fc1b69a9c070876d4
Roland Crompton <rollycrompton@gmail.com>
2023-03-05 12:33:30 +0000

    Made DictionaryTest and implemented tests

---
+++ src/test/DictionaryTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pij.main.Dictionary;
+
+public class DictionaryTest {
+    @Test
+    public void testLookup() {
+        TestUtility.loadDictionary();
+        // Normal words
+        Assertions.assertFalse(Dictionary.lookupWord("a"));
+        Assertions.assertTrue(Dictionary.lookupWord("aa"));
+        // Wildcard words
+        Assertions.assertTrue(Dictionary.lookupWord(" a"));
+        Assertions.assertFalse(Dictionary.lookupWord(" "));
+        Assertions.assertFalse(Dictionary.lookupWord("   Q"));
+
+        Assertions.assertTrue(Dictionary.WILD_CHARACTERS.isEmpty());
+        Dictionary.lookupWord(" a");
+        Assertions.assertFalse(Dictionary.WILD_CHARACTERS.isEmpty());
+        Assertions.assertEquals('a', Dictionary.WILD_CHARACTERS.get(0));
+        
+    }
+
+
+}
