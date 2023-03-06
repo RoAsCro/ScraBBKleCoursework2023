77e9b196c3b8b1b03fa6ff60d38fd56a12547968
Roland Crompton <rollycrompton@gmail.com>
2023-03-06 12:43:03 +0000

    Added testPlaceWildTile to HumanPlayerTest

--- src/test/HumanPlayerTest.java
+++ src/test/HumanPlayerTest.java
@@ -2,9 +2,7 @@ package test;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import pij.main.Board;
-import pij.main.HumanPlayer;
-import pij.main.Player;
+import pij.main.*;
 
 public class HumanPlayerTest {
 
@@ -14,6 +12,21 @@ public class HumanPlayerTest {
         Player player = new HumanPlayer(board);
         Assertions.assertEquals(player.getName(), "Human");
 
+    }
+
+    @Test
+    public void testPlaceWildWord() {
+        Board board = TestUtility.loadBoardFromTestBoards("testBoard.txt");
+        TestUtility.loadDictionary();
+        Assertions.assertTrue(Dictionary.lookupWord("da"));
+        Bag riggedBag = new Bag(new int[]{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 });
+        board.placeTile(board.getCentre(), new LetterTile("A", 1));
+        Player player = new SingleMovePlayer(board, "d,h7,d");
+        player.draw(riggedBag);
+
+        player.turn(riggedBag);
+        Assertions.assertEquals("d3", board.tileAt(new Coordinate('h', 7)).toString());
+        board.print();
     }
 
 }
