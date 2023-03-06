ce7d8d7cee0d1110ac9705512f422a1f52147811
Roland Crompton <rollycrompton@gmail.com>
2023-03-06 14:06:05 +0000

    Place SingleMovePlayer ni HumanPlayerTest and added additional wildcard tile tests

--- src/test/HumanPlayerTest.java
+++ src/test/HumanPlayerTest.java
@@ -6,6 +6,35 @@ import pij.main.*;
 
 public class HumanPlayerTest {
 
+    public class SingleMovePlayer extends Player {
+
+        private String moveString;
+
+        public SingleMovePlayer(Board board, String move) {
+            super(board);
+            this.moveString = move;
+
+        }
+
+        @Override
+        public String getName() {
+            return "SINGLE";
+        }
+
+        @Override
+        public Move turn(Bag bag) {
+            Move move = new Move(this, getBoard());
+            move.tryMove(this.moveString);
+            draw(bag);
+            return move;
+        }
+
+        public void setMoveString(String moveString) {
+            this.moveString = moveString;
+        }
+    }
+
+
     @Test
     public void testGetName() {
         Board board = TestUtility.loadBoardFromTestBoards("testBoard.txt");
@@ -21,12 +50,23 @@ public class HumanPlayerTest {
         Assertions.assertTrue(Dictionary.lookupWord("da"));
         Bag riggedBag = new Bag(new int[]{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 });
         board.placeTile(board.getCentre(), new LetterTile("A", 1));
-        Player player = new SingleMovePlayer(board, "d,h7,d");
+        SingleMovePlayer player = new SingleMovePlayer(board, "d,h7,d");
+//        Assertions.assertTrue();
         player.draw(riggedBag);
 
         player.turn(riggedBag);
         Assertions.assertEquals("d3", board.tileAt(new Coordinate('h', 7)).toString());
-        board.print();
+
+        player.turn(riggedBag);
+        Assertions.assertEquals("d3", board.tileAt(new Coordinate('h', 9)).toString());
+
+        player.setMoveString("q,h7,d");
+        player.turn(riggedBag);
+        Assertions.assertNotEquals("d3", board.tileAt(new Coordinate('h', 10)).toString());
+
+        Assertions.assertEquals(' ', player.getRack().get(0).getChar());
+
+
     }
 
 }
