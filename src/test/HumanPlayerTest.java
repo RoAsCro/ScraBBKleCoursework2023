92b39e9bea9fb3c6d9d384b3449dbf23127f7c83
Roland Crompton <rollycrompton@gmail.com>
2023-03-06 17:52:02 +0000

    Wrote a new test for HumanPlayer

--- src/test/HumanPlayerTest.java
+++ src/test/HumanPlayerTest.java
@@ -1,10 +1,13 @@
 package test;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pij.main.*;
 
 public class HumanPlayerTest {
+
+    private Board board;
 
     public class SingleMovePlayer extends Player {
 
@@ -35,34 +38,66 @@ public class HumanPlayerTest {
     }
 
 
+
+    @BeforeEach
+    public void setUp(){
+        this.board = TestUtility.loadBoardFromTestBoards("testBoard.txt");
+        TestUtility.loadDictionary();
+    }
+
+
     @Test
     public void testGetName() {
-        Board board = TestUtility.loadBoardFromTestBoards("testBoard.txt");
-        Player player = new HumanPlayer(board);
+        Player player = new HumanPlayer(this.board);
         Assertions.assertEquals(player.getName(), "Human");
 
     }
 
     @Test
+    public void testPlaceWord() {
+        SingleMovePlayer player = new SingleMovePlayer(this.board, "AA,a1,d");
+        Bag riggedBag = new Bag(new int[]{7});
+        player.draw(riggedBag);
+        player.turn(riggedBag);
+        Assertions.assertNotEquals(1, board.tileAt(new Coordinate('a', 1)).getValue());
+
+        player.setMoveString(",,");
+        player.turn(riggedBag);
+        player.setMoveString("AA,h8,d");
+        player.turn(riggedBag);
+        Assertions.assertEquals(1, board.tileAt(board.getCentre()).getValue());
+
+        player.setMoveString("A,g8,r");
+        player.turn(riggedBag);
+        Assertions.assertEquals(1, board.tileAt(new Coordinate('g', 8)).getValue());
+
+        player.setMoveString("A,i9,r");
+        player.turn(riggedBag);
+        Assertions.assertNotEquals(1, board.tileAt(new Coordinate('i', 9)).getValue());
+
+        player.setMoveString("A,h9,r");
+        player.turn(riggedBag);
+        Assertions.assertEquals(1, board.tileAt(new Coordinate('i', 9)).getValue());
+        board.print();
+
+    }
+
+    @Test
     public void testPlaceWildWord() {
-        Board board = TestUtility.loadBoardFromTestBoards("testBoard.txt");
-        TestUtility.loadDictionary();
-        Assertions.assertTrue(Dictionary.lookupWord("da"));
         Bag riggedBag = new Bag(new int[]{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 });
-        board.placeTile(board.getCentre(), new LetterTile("A", 1));
-        SingleMovePlayer player = new SingleMovePlayer(board, "d,h7,d");
-//        Assertions.assertTrue();
+        this.board.placeTile(this.board.getCentre(), new LetterTile("A", 1));
+        SingleMovePlayer player = new SingleMovePlayer(this.board, "d,h7,d");
         player.draw(riggedBag);
 
         player.turn(riggedBag);
-        Assertions.assertEquals("d3", board.tileAt(new Coordinate('h', 7)).toString());
+        Assertions.assertEquals("d3", this.board.tileAt(new Coordinate('h', 7)).toString());
 
         player.turn(riggedBag);
-        Assertions.assertEquals("d3", board.tileAt(new Coordinate('h', 9)).toString());
+        Assertions.assertEquals("d3", this.board.tileAt(new Coordinate('h', 9)).toString());
 
         player.setMoveString("q,h7,d");
         player.turn(riggedBag);
-        Assertions.assertNotEquals("d3", board.tileAt(new Coordinate('h', 10)).toString());
+        Assertions.assertNotEquals("d3", this.board.tileAt(new Coordinate('h', 10)).toString());
 
         Assertions.assertEquals(' ', player.getRack().get(0).getChar());
 
