cbe161e0f949fb8b576128846a26c8caab28d639
Roland Crompton <rollycrompton@gmail.com>
2023-03-08 21:37:00 +0000

    Revert "Made Dictionary finale, it's consturctor private and finalised its fields"
    This reverts commit 459e92b47f0c48266554896a9ca22d64178f7d91.

--- src/pij/main/Dictionary.java
+++ src/pij/main/Dictionary.java
@@ -3,7 +3,7 @@ package pij.main;
 import java.io.*;
 import java.util.*;
 
-public final class Dictionary{
+public class Dictionary{
 
     private static final Comparator<String> PREFIX_COMPARATOR = (o1, o2) -> {
         int len1 = o1.length();
@@ -30,14 +30,13 @@ public final class Dictionary{
         return o1.compareTo(o2);
     };
 
-    private static final TreeSet<String> dictionary = new TreeSet<>();
-    private static final TreeSet<String> suffixDictionary = new TreeSet<>(PREFIX_COMPARATOR);
-    private static final TreeSet<String> prefixDictionary = new TreeSet<>(PREFIX_COMPARATOR);
-
-    private Dictionary() {}
+    private static TreeSet<String> dictionary = new TreeSet<>();
+    private static TreeSet<String> suffixDictionary = new TreeSet<>(PREFIX_COMPARATOR);
+    private static TreeSet<String> prefixDictionary = new TreeSet<>(PREFIX_COMPARATOR);
 
     public static void loadDictionary(File file) {
 
+        dictionary = new TreeSet<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
 
