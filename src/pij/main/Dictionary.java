857ce5ca3e0e83539d5b9e1b9b4269510d0f4991
Roland Crompton <rollycrompton@gmail.com>
2023-03-13 15:06:50 +0000

    Cleaned up and wrote documentation for Dictionary

--- src/pij/main/Dictionary.java
+++ src/pij/main/Dictionary.java
@@ -4,39 +4,58 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 /**
- * A dictionary for looking up words in ScraBBKle.
+ * A static dictionary for looking up words in ScraBBKle.
  *
  * @author Roland Crompton
  */
 public final class Dictionary{
 
-    private static final TreeSet<String> dictionary = new TreeSet<>();
+    /**
+     * A TreeSet containing all the possible words. Must be loaded on program start.
+     */
+    private static final SortedSet<String> DICTIONARY = new TreeSet<>();
 
+    /**
+     * Private constructor. This class should not be instantiated.
+     */
     private Dictionary(){}
 
-    public static void loadDictionary(File file) {
+    /**
+     * Loads the default dictionary for ScraBBKle.
+     */
+    public static void loadDictionary() {
+        loadDictionary(new File("../resources/wordlist.txt"));
+    }
 
-        dictionary.clear();
+    /**
+     * Loads a dictionary from the given file, storing it in the dictionary field.
+     *
+     * @param file the File to be loaded from
+     */
+    public static void loadDictionary(File file) {
+        DICTIONARY.clear();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
-
             while ((line = reader.readLine()) != null) {
-                dictionary.add(line);
+                DICTIONARY.add(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void loadDictionary() {
-        loadDictionary(new File("../resources/wordlist.txt"));
-    }
-
+    /**
+     * Looks up a given String in the dictionary.
+     *
+     * @param word the word to be looked up
+     * @return true if the dictionary contains the word, false otherwise
+     */
     public static boolean lookupWord(String word) {
-        return dictionary.contains(word.toLowerCase());
+        return DICTIONARY.contains(word.toLowerCase());
     }
 
 }
