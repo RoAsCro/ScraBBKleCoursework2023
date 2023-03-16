c185bd56fff286ebc5ef85dea2dfcc6779fd7498
Roland Crompton <rollycrompton@gmail.com>
2023-03-16 10:02:09 +0000

    Created LetterTileBonusTest and tests

---
+++ src/test/BonusLetterTileTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pij.main.Tile;
+import pij.main.Tiles.BonusLetterTile;
+import pij.main.Tiles.LetterTile;
+import pij.main.Word;
+
+public class BonusLetterTileTest {
+
+    @Test
+    public void testGettersAndToString() {
+        BonusLetterTile bonus = new BonusLetterTile(3);
+        Assertions.assertEquals("(3)", bonus.toString());
+        Assertions.assertEquals(3, bonus.getValue());
+    }
+
+    @Test
+    public void testAddToSequence() {
+        BonusLetterTile bonus = new BonusLetterTile(3);
+        Tile letter = new LetterTile("A", 1);
+        Word word = new Word();
+        word.addTile(bonus);
+        word.finalise();
+        Assertions.assertEquals(0, word.getScore());
+        word.addTile(letter);
+        word.addTile(bonus);
+        word.finalise();
+        Assertions.assertEquals(3, word.getScore());
+        Assertions.assertEquals("A", word.toString());
+    }
+
+}
