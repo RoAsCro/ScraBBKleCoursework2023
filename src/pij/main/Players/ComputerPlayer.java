9ccf39063773d1221789f35a56d72dcf20468eb4
Roland Crompton <rollycrompton@gmail.com>
2023-03-16 11:33:01 +0000

    Wrote documentation for ComputerPlayer

--- src/pij/main/Players/ComputerPlayer.java
+++ src/pij/main/Players/ComputerPlayer.java
@@ -2,7 +2,6 @@ package pij.main.Players;
 
 import pij.main.*;
 
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -17,16 +16,31 @@ public class ComputerPlayer extends Player {
 	/** The ComputerPlayer's name. */
 	private static final String NAME = "Computer";
 
-	/** The ComputerPlayer's name. */
+	/**
+	 * Constructs a new ComputerPlayer with the given Board.
+	 *
+	 * @param board the Board the ComputerPlayer will play on and use to decide on its Moves
+	 */
 	public ComputerPlayer(Board board) {
 		super(board);
 	}
 
+	/**
+	 * Returns the ComputerPlayer's name "Computer".
+	 *
+	 * @return the ComputerPlayer's name
+	 */
 	@Override
 	public String getName() {
 		return NAME;
 	}
 
+	/**
+	 * Decides on and makes the Move for the ComputerPlayer. Finds all possible Moves
+	 * using the MoveFinder, then finds the highest scoring Move and makes it.
+	 *
+	 * @return the Move the ComputerPlayer makes
+	 */
 	@Override
 	public Move turn() {
 		List<Move> moves = MoveFinder.findMoves(getBoard(), this.getRack(), false);
