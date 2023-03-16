2def245397030a234c04d50f7e814c85b822bb42
Roland Crompton <rollycrompton@gmail.com>
2023-03-16 09:57:08 +0000

    Created a NullBoardTileTest class along with tests

---
+++ src/test/NullBoardTileTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pij.main.Tiles.NullBoardTile;
+import pij.main.Word;
+
+public class NullBoardTileTest {
+
+    @Test
+    public void testGetAndToString() {
+        Assertions.assertEquals(".", new NullBoardTile().toString());
+        Assertions.assertEquals(0, new NullBoardTile().getValue());
+    }
+
+    @Test
+    public void testAddToSequence() {
+        Word word = new Word();
+        word.addTile(new NullBoardTile());
+        word.finalise();
+        Assertions.assertTrue(word.getTiles().isEmpty());
+        Assertions.assertEquals(0, word.getScore());
+        Assertions.assertEquals("", word.toString());
+    }
+
+}
