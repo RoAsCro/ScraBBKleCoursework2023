76fd0147d170399eb443bd2b32508aa35058472a
Roland Crompton <rollycrompton@gmail.com>
2023-03-16 10:03:32 +0000

    Fixed a bug where BonusLetterTiles would crash the program when they're added to empty Words

--- src/pij/main/Tiles/BonusLetterTile.java
+++ src/pij/main/Tiles/BonusLetterTile.java
@@ -27,8 +27,10 @@ public class BonusLetterTile extends AbstractBoardTile {
      */
     @Override
     public void addToSequence(TileSequence tileSequence) {
-        int letterValue = tileSequence.getTiles().getLast().getValue();
-        tileSequence.increaseBaseScore(letterValue * getValue() - letterValue);
+        if (!tileSequence.getTiles().isEmpty()) {
+            int letterValue = tileSequence.getTiles().getLast().getValue();
+            tileSequence.increaseBaseScore(letterValue * getValue() - letterValue);
+        }
     }
 
 }
